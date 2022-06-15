5ed9b46464c2017019d796fa3804afec807ed5ca
Aleksandrino04 <106624793+Aleksandrino04@users.noreply.github.com>
2022-06-15 16:58:56 +0300

    Add files via upload

---
+++ 07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Max_Sequence_of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+            int bestNumber = numbers[0];
+            int currentSequence = 1;
+            int bestSequence = 1;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int currentNumber = numbers[i];
+                int nextNumber = numbers[i+1];
+                if (currentNumber == nextNumber)
+                {
+
+                    currentSequence++;
+                    if (currentSequence > bestSequence)
+                    {
+                        bestSequence = currentSequence;
+                        bestNumber = currentNumber;
+                    }
+                }
+                else
+                {
+                    currentSequence = 1;
+                }
+            }
+            for (int i = 0; i < bestSequence; i++)
+            {
+                Console.Write(bestNumber + " ");
+            }
+
+
+
+        }
+    }
+}
---
+++ 08. Magic Sum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+            int magicNumber = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == magicNumber)
+                    {
+                        Console.WriteLine($"{numbers[i]} {numbers[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
